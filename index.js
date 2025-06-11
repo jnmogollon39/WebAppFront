e7abec981a4aed0a6eadcf5c3773e0c018618a69
laugonzalezo <jnmogollon39@ucatolica.edu.co>
2025-06-11 18:51:43 -0500

    change to http server

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const https = require('http');
+const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = https.createServer((req, res) => {
+const server = http.createServer((req, res) => {
     res.StatusCode = 200;
     res.setHeader('Content-Type', 'test/plain');
     res.end('Hola mundo');
 });
 
 server.listen(port, hostname, () => {
-    console.log('Servidor escuchando en https://${hostname}:${port}/');
+    console.log('Servidor escuchando en http://${hostname}:${port}/');
 })
