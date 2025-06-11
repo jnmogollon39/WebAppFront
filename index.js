76dd1a052c06154de5399d30c720e320686323a7
laugonzalezo <jnmogollon39@ucatolica.edu.co>
2025-06-11 18:42:34 -0500

    hola mundo server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hola mundo Node!');
+const https = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = https.createServer((req, res) => {
+    res.StatusCode = 200;
+    res.setHeader('Content-Type', 'test/plain');
+    res.end('Hola mundo');
+});
+
+server.listen(port, hostname, () => {
+    console.log('Servidor escuchando en https://${hostname}:${port}/');
+})
