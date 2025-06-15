f9e59d2ae3ebe7d0f0f0a29f28164e8c07fe5cbc
laugonzalezo <jnmogollon39@ucatolica.edu.co>
2025-06-15 15:36:13 -0500

    correxión statuscode

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const hostname = '127.0.0.1';
 const port = process.env.port || 3000;
 
 const server = http.createServer((req, res) => {
-    res.StatusCode = 200;
+    res.statusCode = 200;
     const msg = 'Hola mundo\n';
     res.end(msg);
 });
