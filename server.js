b13d9984c244e43ddeda64614edbaedf5a3592b7
laugonzalezo <jnmogollon39@ucatolica.edu.co>
2025-06-16 09:45:28 -0500

    add server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+// Servir los archivos estáticos del build
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Todas las rutas deben devolver index.html (para React Router)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
